e5e4a7eec964421e8f4edff58ddd4647d3bd0b23
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-04-17 20:38:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from classes import BissectionMethod
+import time
+
+function = input("Please, insert a function: ")
+print("Please, insert a interval [a, b]:")
+a = int(input("- a: "))
+b = int(input("- b: "))
+stop = int(input("Insert an 'n' for 10**(-n): "))
+
+print(f"Interval: [{a}, {b}] | Stop parameter: 10**(-{stop})")
+
+start = time.time()
+
+obj = BissectionMethod(function)
+result, root = obj.bissect(a, b, stop)
+obj.printTable()
+print(f"The {root} is x={result}")
+obj.plotGraph(a, b)
+
+end = time.time()
+
+final = end - start
+print(final)
