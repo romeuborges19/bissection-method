09be381d1454649cb404ecbcd69fcbd7af647130
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-04-18 10:00:01 -0300

    Small adjusts

--- classes.py
+++ classes.py
@@ -2,44 +2,37 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class BissectionMethod():
     def __init__(self, function):
         self.f = lambda x: eval(function)
         self.table = []
         self.valx = []
 
-    def printFunction(self, x):
-        print(self.f(x))
-
     def bissect(self, a, b, stop, n=1):
-        if n == 1 and self.f(a)*self.f(b) >= 0:
-            print("Invalid interval")
-        else:
-            middle = (a+b)/2
-            self.valx.append(middle)
-            param = 10**(-stop)
-            
-            if n-2 in range(len(self.valx)):
-                epsilon = abs(self.valx[n-1] - self.valx[n-2])
-                self.table.append([n, '{:.5f}'.format(a), 
-                                  '{:.5f}'.format(b), 
-                                  '{:.5f}'.format(middle), 
-                                  '{:.5f}'.format(self.f(middle)), 
-                                  '{:.5f}'.format(epsilon)])
-                if epsilon < param:
-                    return middle, "approximate root"
-            else:
-                self.table.append([n, a, b, middle, self.f(middle), '--'])
-            
-            if self.f(middle) == 0:
-                return middle, "exact root"
-            
-            if self.f(a)*self.f(middle) < 0:
-                return self.bissect(a, middle, stop, n+1)
-            else:
-                return self.bissect(middle, b, stop, n+1)
+        middle = (a+b)/2
+        self.valx.append(middle)
+        param = 10**(-stop)
         
+        if n-2 in range(len(self.valx)):
+            epsilon = abs(self.valx[n-1] - self.valx[n-2])
+            self.table.append([n, '{:.5f}'.format(a), 
+                              '{:.5f}'.format(b), 
+                              '{:.5f}'.format(middle), 
+                              '{:.5f}'.format(self.f(middle)), 
+                              '{:.5f}'.format(epsilon)])
+            if epsilon < param:
+                return middle, "approximate root"
+        else:
+            self.table.append([n, a, b, middle, self.f(middle), '--'])
+        
+        if self.f(middle) == 0:
+            return middle, "exact root"
+        
+        if self.f(a)*self.f(middle) < 0:
+            return self.bissect(a, middle, stop, n+1)
+        else:
+            return self.bissect(middle, b, stop, n+1)
+    
     def printTable(self):
         df = pd.DataFrame(columns=['n', 'An', 'Bn', 'Xn', 'f(Xn)', 'E'], 
                           data=self.table)
@@ -47,10 +40,10 @@ class BissectionMethod():
 
     def plotGraph(self, a, b):
         x = np.linspace(a, b, num=100)
-        y = np.array(self.f(x))
+        
         plt.figure()
         plt.grid()
-        plt.plot(x, y)
+        plt.plot(x, self.f(x))
 
         cont = 0
         for x in self.valx:
@@ -60,5 +53,3 @@ class BissectionMethod():
             cont=cont+1
 
         plt.show()
-
-
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from classes import BissectionMethod
 import time
 
-function = input("Please, insert a function: ")
-print("Please, insert a interval [a, b]:")
-a = int(input("- a: "))
-b = int(input("- b: "))
-stop = int(input("Insert an 'n' for 10**(-n): "))
+def main():
 
-print(f"Interval: [{a}, {b}] | Stop parameter: 10**(-{stop})")
+    function = input("Please, insert a function: ")
 
-obj = BissectionMethod(function)
-result, root = obj.bissect(a, b, stop)
-obj.printTable()
-print(f"The {root} is x={result}")
-obj.plotGraph(a, b)
+    obj = BissectionMethod(function)
+
+    while True:
+        print("Please, insert a valid interval [a, b]:")
+        a = int(input("- a: "))
+        b = int(input("- b: "))
+        if obj.f(a)*obj.f(b) < 0:
+            break
+
+    stop = int(input("Insert an 'n' for 10**(-n): "))
+
+
+    print(f"Interval: [{a}, {b}] | Stop parameter: 10**(-{stop})")
+
+    result, root = obj.bissect(a, b, stop)
+    obj.printTable()
+    print(f"The {root} is x={result}")
+    obj.plotGraph(a, b)
+
+if __name__=='__main__':
+    main()
